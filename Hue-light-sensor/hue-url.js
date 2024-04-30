5be2a8d92aa6f3d0ba372c9ebaba06daa762bcfa
ManalAbdulqawi <denever_2008@windowslive.com>
2024-04-30 10:27:23 +0100

    organise files

---
+++ Hue-light-sensor/hue-url.js
@@ -0,0 +1,4 @@
+const host = "http://192.168.1.174";
+const usrename = "SsuLdmWXsPOePgsIOCCn1puM61eh73TPBjKbe8pS";
+const hueUrl = `${host}/api/${usrename}`;
+module.exports = { host, usrename, hueUrl };
