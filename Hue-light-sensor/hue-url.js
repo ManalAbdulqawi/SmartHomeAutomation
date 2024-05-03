6841aaf8404f17a4a60dd2ec84bf1ab035f6f6bc
ManalAbdulqawi <denever_2008@windowslive.com>
2024-05-03 09:42:09 +0100

    organise Hueautomation filse

--- Hue-light-sensor/hue-url.js
+++ Hue-light-sensor/hue-url.js
@@ -1,4 +1,4 @@
-const host = "your hue bridge ip addres";
-const usrename = "your hue bridge username";
+const host = "your hue ip address";
+const usrename = "your hue username";
 const hueUrl = `${host}/api/${usrename}`;
 module.exports = { host, usrename, hueUrl };
